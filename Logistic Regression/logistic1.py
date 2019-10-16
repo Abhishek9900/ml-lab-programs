a560a3389e9c4de2ca63de5600fbd909e0c581b0
Abhilash Pani <abhilashpani651@gmail.com>
2019-10-17 02:11:05 +0530

    Correctly imported libraries

--- Logistic Regression/logistic1.py
+++ Logistic Regression/logistic1.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
 from sklearn.metrics import roc_curve, auc
 from sklearn import datasets
 from sklearn.multiclass import OneVsRestClassifier
